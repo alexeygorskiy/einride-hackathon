0f14e9d1e420c86a1a3f66c90e5b2b10df4c6b98
Mattias <klutten99@gmail.com>
2021-11-27 11:35:53 +0100

    show_hough in car

--- car.py
+++ car.py
@@ -2,12 +2,46 @@ import websocket
 import cv2
 import numpy as np
 import sys
+from IPython.display import display
+from PIL import Image
 
 host = "192.168.245.23"
 port = 8887
 
 socket_address = f"ws://{host}:{port}/wsDrive"
 video_address = f"http://{host}:{port}/video"
+
+def show_hough(frame):
+    frame = frame[60:120,10:140,:]
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    kernel_size = 5
+    blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+    low_threshold = 190
+    high_threshold = 200
+    edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+    rho = 1  # distance resolution in pixels of the Hough grid
+    theta = np.pi / 180  # angular resolution in radians of the Hough grid
+    threshold = 40  # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 40  # minimum number of pixels making up a line
+    max_line_gap = 10  # maximum gap in pixels between connectable line segments
+    line_image = np.copy(frame) * 0  # creating a blank to draw lines on
+
+    # Run Hough on edge detected image
+    # Output "lines" is an array containing endpoints of detected line segments
+    lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                        min_line_length, max_line_gap)
+
+    if type(lines) is np.ndarray:
+        print(lines)
+        for line in lines:
+            for x1,y1,x2,y2 in line:
+                cv2.line(line_image,(x1,y1),(x2,y2),(0,0,255),1)
+                cv2.circle(line_image, (x1,y1), radius=2, color=(255, 0, 0), thickness=3)
+                cv2.circle(line_image, (x2,y2), radius=2, color=(255, 0, 0), thickness=3)
+                pass
+        
+        display(Image.fromarray(line_image))
 
 def on_message(ws, message):
     print(message)
@@ -35,18 +69,20 @@ def on_open(ws):
     while True:
         try:
             ret, frame = cap.read()
+            if ret:
+                show_hough(frame)
             angle = 0
-            throttle = 0.5
+            throttle = 0
 
             message = f"{{\"angle\":{angle},\"throttle\":{throttle},\"drive_mode\":\"user\",\"recording\":false}}"
             ws.send(message)
-            print(message)
+            #print(message)
         except Exception as error:
             on_error(ws, error)
 
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
+    websocket.enableTrace(False)
     ws = websocket.WebSocketApp(socket_address,
                                 on_open=on_open,
                                 on_message=on_message,
--- hough_calib.py
+++ hough_calib.py
@@ -4,36 +4,46 @@ import numpy as np
 from IPython.display import display
 
 frame = cv2.imread('src.png')
+frame = frame[60:120,10:140,:]
 gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
 kernel_size = 5
 blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
-low_threshold = 50
-high_threshold = 150
+low_threshold = 190
+high_threshold = 200
 edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 rho = 1  # distance resolution in pixels of the Hough grid
 theta = np.pi / 180  # angular resolution in radians of the Hough grid
-threshold = 15  # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 50  # minimum number of pixels making up a line
-max_line_gap = 20  # maximum gap in pixels between connectable line segments
+threshold = 40  # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 40  # minimum number of pixels making up a line
+max_line_gap = 10  # maximum gap in pixels between connectable line segments
 line_image = np.copy(frame) * 0  # creating a blank to draw lines on
 
 # Run Hough on edge detected image
 # Output "lines" is an array containing endpoints of detected line segments
 lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
                     min_line_length, max_line_gap)
+print(lines)
 for line in lines:
     for x1,y1,x2,y2 in line:
-        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+        cv2.line(line_image,(x1,y1),(x2,y2),(0,0,255),1)
+        pass
+
 
 # Draw the lines on the  image
-lines_edges = cv2.addWeighted(frame, 0.8, line_image, 1, 0)
+contours, hierarchy = cv2.findContours(image=edges, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 
-#cv2.imshow('image',lines_edges)
-#cv2.waitKey(0)
+if contours is not None and len(contours) > 0:
+    C = max(contours, key = cv2.contourArea)
 
-display(Image.fromarray(frame))
-display(Image.fromarray(gray))
-display(Image.fromarray(blur_gray))
+    rect = cv2.minAreaRect(C)
+    box = cv2.boxPoints(rect)
+#cv2.drawContours(image=line_image, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=1, lineType=cv2.LINE_AA)
+#print(contours)
+#print(hierarchy)
+#lines_edges = cv2.addWeighted(frame, 0.8, line_image, 1, 0)
+#display(Image.fromarray(frame))
+#display(Image.fromarray(gray))
+#display(Image.fromarray(blur_gray))
+#display(Image.fromarray(line_image))
 display(Image.fromarray(line_image))
-display(Image.fromarray(lines_edges))
