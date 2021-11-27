faedd560f48c60e3cd2235920f065b61b193926e
Mattias <klutten99@gmail.com>
2021-11-27 09:54:05 +0100

    Update hough_calib.py

--- hough_calib.py
+++ hough_calib.py
@@ -1,8 +1,10 @@
 import cv2
+from PIL import Image
 import numpy as np
+from IPython.display import display
 
-img = cv2.imread('src.png')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+frame = cv2.imread('src.png')
+gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
 kernel_size = 5
 blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
@@ -14,7 +16,7 @@ theta = np.pi / 180  # angular resolution in radians of the Hough grid
 threshold = 15  # minimum number of votes (intersections in Hough grid cell)
 min_line_length = 50  # minimum number of pixels making up a line
 max_line_gap = 20  # maximum gap in pixels between connectable line segments
-line_image = np.copy(img) * 0  # creating a blank to draw lines on
+line_image = np.copy(frame) * 0  # creating a blank to draw lines on
 
 # Run Hough on edge detected image
 # Output "lines" is an array containing endpoints of detected line segments
@@ -25,7 +27,13 @@ for line in lines:
         cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
 
 # Draw the lines on the  image
-lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
+lines_edges = cv2.addWeighted(frame, 0.8, line_image, 1, 0)
 
-cv2.imshow('image',edges)
-cv2.waitKey(0)
+#cv2.imshow('image',lines_edges)
+#cv2.waitKey(0)
+
+display(Image.fromarray(frame))
+display(Image.fromarray(gray))
+display(Image.fromarray(blur_gray))
+display(Image.fromarray(line_image))
+display(Image.fromarray(lines_edges))
